92341965924021f33ceb0a2eddf85c4823906744
HoangKhoi <thienkhoi0604@gmail.com>
2022-09-11 15:20:47 +0700

    Components React Part 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from "./logo.svg";
-import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
+import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { useState } from "react";
 
 function App() {
+  const [dishes, setDishes] = useState(DISHES);
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,7 +14,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={dishes} />
     </div>
   );
 }
