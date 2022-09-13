a5b32952a4a8aac923541c0fce83f6278909eead
HoangKhoi <thienkhoi0604@gmail.com>
2022-09-13 16:15:21 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,20 +2,39 @@
 import { useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 
 function Main() {
   const [dishes, setDishes] = useState(DISHES);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [comments, setComments] = useState(COMMENTS);
+  const [leaders, setLeaders] = useState(LEADERS);
 
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <Home
+              dish={dishes.filter((dish) => dish.featured)[0]}
+              promotion={
+                promotions.filter((promotion) => promotion.featured)[0]
+              }
+              leader={leaders.filter((leader) => leader.featured)[0]}
+            />
+          }
+        />
         <Route exact path="/menu" element={<Menu dishes={dishes} />} />
+        <Route exact path="/contactus" element={<Contact />} />
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
       <Footer />
