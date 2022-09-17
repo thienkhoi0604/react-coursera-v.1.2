8b63ce4199da735cb2c4027e70e69222f684e263
HoangKhoi <thienkhoi0604@gmail.com>
2022-09-17 11:08:08 +0700

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,8 +6,135 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+
+const CommentForm = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.yourName) {
+      errors.yourName = "Required";
+    } else if (values.yourName.length < 3) {
+      errors.yourName = "This field should at least be three characters long.";
+    } else if (values.yourName.length >= 15) {
+      errors.yourName = "Must be less than or equal to 15 characters.";
+    } else {
+      //Do nothing
+    }
+
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues: {
+      rating: "1",
+      yourName: "",
+      comment: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <React.Fragment>
+      <Button
+        outline
+        color="secondary"
+        onClick={() => setIsModalOpen(!isModalOpen)}
+      >
+        Submit Comment
+      </Button>
+      <Modal isOpen={isModalOpen} toggle={() => setIsModalOpen(!isModalOpen)}>
+        <ModalHeader toggle={() => setIsModalOpen(!isModalOpen)}>
+          Submit Comment
+        </ModalHeader>
+        <ModalBody>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="form-group">
+              <label
+                style={{ "padding-left": 0 }}
+                className="col-12"
+                htmlFor="rating"
+              >
+                Rating
+              </label>
+              <select
+                className="col-12 form-control"
+                id="rating"
+                name="rating"
+                value={formik.values.rating}
+                onChange={formik.handleChange}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label
+                style={{ "padding-left": 0 }}
+                className="col-12 col-form-label"
+                htmlFor="yourName"
+              >
+                Your name
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="yourName"
+                name="yourName"
+                placeholder="Your Name"
+                value={formik.values.yourName}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.errors.yourName ? (
+                <div style={{ color: "red", fontSize: "10px" }}>
+                  {formik.errors.yourName}
+                </div>
+              ) : null}
+            </div>
+            <div className="form-group">
+              <label
+                style={{ "padding-left": 0 }}
+                className="col-12 col-form-label"
+                htmlFor="comment"
+              >
+                Comment
+              </label>
+              <textarea
+                className="form-control"
+                id="comment"
+                name="comment"
+                rows="6"
+                value={formik.values.comment}
+                onChange={formik.handleChange}
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </ModalBody>
+      </Modal>
+    </React.Fragment>
+  );
+};
 
 const CommentDishdetail = ({ comment }) => {
   return (
@@ -56,7 +183,7 @@ const Dishdetail = ({ dish, comments }) => {
             </CardBody>
           </Card>
         </div>
-        <div className="col-12 col-md-5 m-1">
+        <div style={{ textAlign: "left" }} className="col-12 col-md-5 m-1">
           <h4>Comments</h4>
           {comments.map((comment) => {
             return (
@@ -65,11 +192,12 @@ const Dishdetail = ({ dish, comments }) => {
               </div>
             );
           })}
+          <CommentForm />
         </div>
       </div>
     </div>
   ) : (
-    <div></div>
+    <div>Bug</div>
   );
 };
 
